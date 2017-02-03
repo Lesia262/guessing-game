4151e3e4c41d016611bc38624d7b033c7a4d2d9a
lesia262 <f.lesenka@gmail.com>
2017-02-03 21:35:29 +0300

    make first methods

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.minValue = null;
+    	this.maxValue = null;
+
+    }
 
     setRange(min, max) {
-
+    	this.minValue = min;
+    	this.maxValue = max;
     }
 
     guess() {
-
+    	var guessValue = (this.minValue + this.maxValue)/2 - ((this.minValue + this.maxValue)%2)/2;
+    	return guessValue;
     }
 
     lower() {
+		this.maxValue = (this.minValue + this.maxValue)/2 - ((this.minValue + this.maxValue)%2)/2;
 
     }
 
     greater() {
+		this.minValue = (this.minValue + this.maxValue)/2 - ((this.minValue + this.maxValue)%2)/2;
 
     }
 }
